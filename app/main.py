fbfb92842c4bfdb1a2f2d3ce7a45e35e18fe8202
Carcei Ioan <icarcei@yahoo.com>
2024-02-29 12:05:42 +0200

    test update docker on ec2 no1

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ VERSION = "1.0.0"
 @app.get("/")
 async def read_root():
     """function root"""
-    return {"msg": "Hello world"}
+    return {"msg": "Hello world!!!"}
 
 
 @app.get("/name/{name}")
